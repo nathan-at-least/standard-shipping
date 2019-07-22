f49a72ec3c4f4ce726f19754aa98502a0b21f7e7
Nathan Wilcox <nathan+dev@z.cash>
2019-07-21 17:03:26 -0700

    Start commandline parser skeleton w/ clap: there's a "verify" subcommand with an "all" subsubcommand.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::{App, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .subcommand(
+            SubCommand::with_name("verify")
+                .about("Verify aspects of the workspace or package.")
+                .subcommand(
+                    SubCommand::with_name("all")
+                        .about("Run all verifications.")
+                )
+        )
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("verify") {
+        if let Some(_matches) = matches.subcommand_matches("verify") {
+            unimplemented!("verify all.");
+        } else {
+            unimplemented!("No verify subcommand.");
+        }
+    } else {
+        unimplemented!("No top subcommand.");
+    }
 }
